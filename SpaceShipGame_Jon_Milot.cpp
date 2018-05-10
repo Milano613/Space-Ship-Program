8055fded4beb17ea6798d06f84988795fc896e00
Milano613 <39098019+Milano613@users.noreply.github.com>
2018-05-10 12:55:16 -0400

    Initial commit

---
+++ SpaceShipGame_Jon_Milot.cpp
@@ -0,0 +1,28 @@
+/******************************************
+*   Name: Jonathan Lee & Milot Latifi     *
+*                                         *
+*   Course Code: ICS3U                    *
+*                                         *
+*   Date: May 9, 2018                     *
+*                                         *
+*   Summative Project - Battle Ship       *
+*******************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+int main(){
+    system("COLOR E");
+    printf("\n\t\t\t******     ******  ******** ******** ***   ****** ***** **  **  *** *******");
+    printf("\n\t\t\t**   **   **    **   ***      ***    ***   **     *     **  **  *** **    **");
+    printf("\n\t\t\t******   **********  ***      ***    ***   ****** ***** ******  *** *******");
+    printf("\n\t\t\t**   **  **      **  ***      ***    ***   **         * **  **  *** **");
+    printf("\n\t\t\t******   **      **  ***      ***    ***** ****** ***** **  **  *** **");
+    printf("\n\n\n\n\n\n");
+    fflush(stdin);
+
+    return 0;
+}
+
